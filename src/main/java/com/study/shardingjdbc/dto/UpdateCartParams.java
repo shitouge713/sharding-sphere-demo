be70b422661169aaa61cebce2133802d0f392bdb
songxuelei20220606 <835451318@qq.com>
2024-03-08 17:59:08 +0800

    init

--- src/main/java/com/study/shardingjdbc/dto/UpdateCartParams.java
+++ src/main/java/com/study/shardingjdbc/dto/UpdateCartParams.java
@@ -1,5 +1,6 @@
 package com.study.shardingjdbc.dto;
 
+import com.study.shardingjdbc.suanfa.Snowflake;
 import lombok.Data;
 
 /**
@@ -9,4 +10,14 @@ import lombok.Data;
 public class UpdateCartParams {
     private Long id;
     private Integer kcSize;
+
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+        Snowflake snowflake = new Snowflake();
+        for (int i = 0; i < 3; i++) {
+            String snowflakeNextId = snowflake.nextIdStr();
+            System.out.println(snowflakeNextId);
+        }
+
+    }
 }
